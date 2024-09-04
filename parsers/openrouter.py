91a2be419cc72ac6b30fe5ee7945b3232f1ea2b6
c0nstanta <konstantin.koshevoj@bk.ru>
2024-09-04 18:37:47 +0500

    openrouter api

--- parsers/openrouter.py
+++ parsers/openrouter.py
@@ -1,64 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import re
-import os
+import requests
+import json
 
-def openrouter(url="https://openrouter.ai/docs/models"):
-    geckodriver_path = os.getenv('GECKODRIVER_PATH', '/usr/local/bin/geckodriver')
-    firefox_path = os.getenv('FIREFOX_PATH', '/usr/bin/firefox')
+def openrouter(url="https://openrouter.ai/api/v1/models"):
+    result = requests.get(url).content.decode('utf-8')
+    data = json.loads(result)["data"]
+    
+    result = {}
 
-    # Использование путей в коде
-    service = Service(geckodriver_path)
-    options = webdriver.FirefoxOptions()
-    options.binary_location = firefox_path
-    options.add_argument('--headless')
-    driver = webdriver.Firefox(service=service, options=options)
-    driver.get(url)
-    
-    try:
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'table.w-full.table-fixed'))
-        )
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.w-full.table-fixed tbody tr'))
-        )
-    except Exception as e:
-        driver.quit()
-        return {"error": f"Table not found: {str(e)}"}
-    
-    html_content = driver.page_source
-    driver.quit()
-    soup = BeautifulSoup(html_content, 'html.parser')
-    table = soup.find('table', class_='w-full table-fixed')
-    
-    if table is None:
-        return {"error": "Table not found"}
-    
-    tbody = table.find('tbody', class_='divide-y')
-    rows = tbody.find_all('tr', class_='text-sm')
-
-    data = {}
-    for row in rows:
-        title = row.get('title')
-        columns = row.find_all('td')
-        model_name = columns[0].find('a').text.strip()
-        
-        input_div = columns[1].find('div')
-        input_price = input_div.contents[0].strip().replace('$', '') if input_div else 'N/A'
-        
-        output_div = columns[2].find('div')
-        output_price = output_div.contents[0].strip().replace('$', '') if output_div else 'N/A'
-        
-        data[model_name] = {
-            "input_price": input_price,
-            "output_price": output_price
+    for i in range(0, len(data)):
+        result[data[i]["name"]] = {
+            "input_price": str(float(data[i]["pricing"]["prompt"]) * 1000000),
+            "output_price": str(float(data[i]["pricing"]["completion"]) * 1000000)
         }
-    return data
+    return result
 
 if __name__ == "__main__":
     result = openrouter()
-    print(result)
+    with open('openrouter.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(result, ensure_ascii=False, indent=4))
