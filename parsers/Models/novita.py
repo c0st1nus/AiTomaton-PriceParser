132eecca978c8f1d6dd5a623ac53d26c1b36dad1
c0nstanta <konstantin.koshevoj@bk.ru>
2024-11-05 23:17:43 +0500

    novita fix

--- parsers/Models/novita.py
+++ parsers/Models/novita.py
@@ -1,15 +1,15 @@
 import requests
 import time
+import json
 
 def novita(url="https://novita.ai/api/chat-models"):
     start_time = time.time()
     result = requests.get(url)
     result.raise_for_status()
     json_data = result.json()
-    
     data = {}
     
-    for model_data in json_data:
+    for model_data in json_data["data"]:
         model = model_data['title'].split('/')[-1]
         input_price = str(model_data['input_token_price_per_m'] / 10000)
         output_price = str(model_data['output_token_price_per_m'] / 10000)
